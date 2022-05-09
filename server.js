f70aa5521886144790cf43c0d557d77f17018a48
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-05-09 09:58:41 -0700

    adds fetch api

--- server.js
+++ server.js
@@ -157,6 +157,22 @@ function filter_list(l) {
     return l.filter(el=>typeof el == 'number')
 }
  
-console.log(filter_list([1,2,'a','b'])) //== [1,2])
+// console.log(filter_list([1,2,'a','b'])) //== [1,2])
 // console.log(filter_list([1,'a','b',0,15]) == [1,0,15])
 // console.log(filter_list([1,2,'aasf','1','123',123]) == [1,2,123])
+
+
+
+// FETCH
+const endpoint = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+fetch(endpoint)
+    .then(response => {
+        if (!response.ok) {
+            console.log('error connecting')
+        } else {
+            console.log('success')
+            return response.json()
+        }
+    })
+    .then(pokeData => console.log(pokeData.abilities))
